92f77b05fec80c5440a8b800b33345dabca13872
Case Duckworth <acdw@acdw.net>
2022-01-08 14:52:44 -0600

    Upgrade to version 0.2
    Changelog:
    
    - Deprecate `electric-cursor-default-cursor` for a `t` entry in
      `electric-cursor-alist`
    - Add `electric-cursor` customization group
    - Pull cursor customization types from `cursor-type`'s type
    - Refactor cursor determining and setting functions
    - Refactor hook-setting and resetting functions

--- electric-cursor.el
+++ electric-cursor.el
@@ -1,4 +1,4 @@
-;;; electric-cursor.el --- Change cursor automatically -*- lexical-binding: t; -*-
+;;; electric-cursor.el --- Change cursor automatically depending on mode -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2021 Case Duckworth
 ;; This file is NOT part of GNU Emacs.
@@ -6,9 +6,9 @@
 ;; Author: Case Duckworth <acdw@acdw.net>
 ;; License: ISC
 ;; SPDX-License-Identifier: ISC
-;; Version: 0.1.1
-;; Package-Requires: ((emacs "24.1"))
-;; Keywords: cursor, files
+;; Version: 0.2
+;; Package-Requires: ((emacs "25.1"))
+;; Keywords: terminals, frames
 ;; URL: https://github.com/duckwork/electric-cursor
 
 ;;; Commentary:
@@ -26,107 +26,118 @@
 
 ;;; Code:
 
-(defvar electric-cursor-original-cursor nil
-  "The original cursor type before `electric-cursor-mode'.")
+(eval-when-compile
+  (require 'cl-lib))
+
+;;; Variables
+
+(defvar electric-cursor--original-cursor nil
+  "The cursor type before calling function `electric-cursor-mode'.")
+
+;;; Customization options
+
+(defgroup electric-cursor nil
+  "Customizations for electric cursor functionality."
+  :prefix "electric-cursor-"
+  :group 'cursor)
+
+(defcustom electric-cursor-alist '((overwrite-mode . box)
+                                   (t . bar))
+  "Alist of modes and cursors to apply to them.
+The car of each of element is a mode or hook, and the cdr is the
+`cursor-type', which see."
+  :type '(alist :key-type (choice (function :tag "Mode")
+                                  (hook :tag "Mode hook"))
+                :value-type (get 'cursor-type 'custom-type)))
+
+(defcustom electric-cursor-default-cursor (alist-get t electric-cursor-alist)
+  "The cursor to use when no modes in `electric-cursor-alist' are active.
+This option is deprecated in favor of using a t car in
+`electric-cursor-alist'."
+  :type (get 'cursor-type 'custom-type))
+
+(defcustom electric-cursor-set-in-terminal t
+  "Should function `electric-cursor-mode' attempt to set cursor in a terminal?"
+  :type 'boolean)
+
+;;; Internal functions
+
+(defun electric-cursor--determine (&optional type)
+  "Determine the cursor that should be in use.
+When TYPE is supplied, use that; otherwise, find the type from
+`electric-cursor-alist' or `electric-cursor-default-cursor'."
+  (or type
+      (cl-loop for (mode . cursor) in electric-cursor-alist
+               if (or (eq mode t)
+                      (and (boundp mode) (symbol-value mode)))
+               return cursor)
+      electric-cursor-default-cursor))
+
+(defun electric-cursor--apply-to-hooks (fn)
+  "Apply FN to the modes defined in `electric-cursor-alist'."
+  (cl-loop for (mode-or-hook . _) in electric-cursor-alist
+           do (cond ((eq mode-or-hook t) nil)
+                    ((equal (substring (format "%s" mode-or-hook) -5) "-hook")
+                     (funcall fn mode-or-hook #'electric-cursor-set-cursor))
+                    (t (funcall fn (intern (format "%s-hook" mode-or-hook))
+                                #'electric-cursor-set-cursor)))))
+
+;;; Functions
+
+(defun electric-cursor-set-terminal-cursor (&optional type)
+  "Set the cursor in a terminal.
+Set it to TYPE if provided; otherwise, determine the cursor with
+`electric-cursor--determine'."
+  (unless type (setq type (electric-cursor--determine)))
+  (when (and electric-cursor-set-in-terminal
+             (frame-parameter nil 'tty))
+    (send-string-to-terminal
+     (concat "\e["
+             (let ((n (pcase (or (car-safe type) type)
+                        ('box 2)
+                        ('bar 6)
+                        ('hbar 4)
+                        (_ 0))))
+               (number-to-string (if blink-cursor-mode
+                                     (max (1- n) 0)
+                                   n)))
+             " q"))))
+
+(defun electric-cursor-set-cursor (&optional type)
+  "Set the cursor.
+When TYPE is non-nil, set the cursor to that; otherwise,
+determine the cursor with `electric-cursor--determine'."
+  (let ((type (electric-cursor--determine type)))
+    (setq cursor-type type)
+    (electric-cursor-set-terminal-cursor type)))
+
+(defun electric-cursor-add-hooks ()
+  "Add `electric-cursor-set-cursor' to modes in `electric-cursor-alist'."
+  (electric-cursor--apply-to-hooks #'add-hook))
+
+(defun electric-cursor-remove-hooks ()
+  "Remove `electric-cursor-set-cursor' to modes in `electric-cursor-alist'."
+  (electric-cursor--apply-to-hooks #'remove-hook))
+
+;;; Minor mode
 
 ;;;###autoload
 (define-minor-mode electric-cursor-mode
-  "Toggle `electric-cursor-mode'.
-
-This global minor mode adds necessary hooks and functions to
-change the cursor's shape, dependent on the modes defined in
-`electric-cursor-alist'."
+  "Change the cursor automatically depending on mode.
+This global minor mode adds the necessary hooks to modes defined
+in `electric-cursor-alist' to change the cursor's shape when
+entering and exiting those modes.  It also saves the cursor's
+shape and restores it when exiting."
   :lighter " |_"
-  :init-value nil
-  :keymap nil
   :global t
   (if electric-cursor-mode
-      (progn
-        (setq electric-cursor-original-cursor cursor-type)
+      (progn                            ; Enable
+        (setq electric-cursor--original-cursor cursor-type)
         (electric-cursor-set-cursor)
         (electric-cursor-add-hooks))
-    (setq cursor-type electric-cursor-original-cursor)
-    (unless (display-graphic-p)
-      (send-string-to-terminal "\e[0 q"))
-    (electric-cursor-remove-hooks)))
-
-(defcustom electric-cursor-alist '((overwrite-mode . box))
-  "The alist of modes and cursors for function `electric-cursor-mode'.
-
-The CAR of each element is the mode, and the CONS is the `cursor-type'."
-  :type '(alist
-          :value-type (choice
-                       (const :tag "Frame default" t)
-                       (const :tag "Filled box" box)
-                       (const :tag "Hollow cursor" hollow)
-                       (const :tag "Vertical bar" bar)
-                       (const :tag "Vertical bar with specified width"
-                              (const bar) integer)
-                       (const :tag "Horizontal bar" hbar)
-                       (const :tag "Horizontal bar with specified width"
-                              (const hbar) integer)
-                       (const :tag "None" nil)))
-  :group 'electric-cursor)
-
-(defcustom electric-cursor-default-cursor 'bar
-  "The cursor to use when no modes in `electric-cursor-alist' apply."
-  :type '(choice
-          (const :tag "Frame default" t)
-          (const :tag "Filled box" box)
-          (const :tag "Hollow cursor" hollow)
-          (const :tag "Vertical bar" bar)
-          (const :tag "Vertical bar with specified width"
-                 (const bar) integer)
-          (const :tag "Horizontal bar" hbar)
-          (const :tag "Horizontal bar with specified width"
-                 (const hbar) integer)
-          (const :tag "None" nil))
-  :group 'electric-cursor)
-
-(defcustom electric-cursor-set-in-terminal t
-  "Whether to set the cursor in a terminal."
-  :type 'boolean
-  :group 'electric-cursor)
-
-(defun electric-cursor-set-cursor ()
-  "Set the cursor according to `electric-cursor-alist'."
-  (let ((electric-cursor-type
-         (or (catch :found
-               (dolist (spec electric-cursor-alist)
-                 (when (and (boundp (car spec))
-                            (car spec))
-                   (throw :found (cdr spec)))))
-             electric-cursor-default-cursor)))
-    (cond ((display-graphic-p)
-           (setq cursor-type electric-cursor-type))
-          (electric-cursor-set-in-terminal
-           (send-string-to-terminal
-            (concat "\e["
-                    (let ((n (pcase (or (car-safe electric-cursor-type)
-                                        electric-cursor-type)
-                               ('box 2)
-                               ('bar 6)
-                               ('hbar 4)
-                               (_ 0))))
-                      (number-to-string (if blink-cursor-mode
-                                            (max (1- n) 0)
-                                          n)))
-                    " q"))))))
-
-(defun electric-cursor-add-hooks ()
-  "Add hooks to the modes defined in `electric-cursor-alist'."
-  (dolist (spec electric-cursor-alist)
-    (unless (eq (car spec) t)
-      (let ((hook (intern (concat (symbol-name (car spec)) "-hook"))))
-        (add-hook hook #'electric-cursor-set-cursor)))))
-
-(defun electric-cursor-remove-hooks ()
-  "Remove hooks from the modes defined in `electric-cursor-alist'."
-  (dolist (spec electric-cursor-alist)
-    (unless (eq (car spec) t)
-      (let ((hook (intern (concat (symbol-name (car spec)) "-hook"))))
-        (remove-hook hook #'electric-cursor-set-cursor)))))
+    ;; Disable
+    (electric-cursor-remove-hooks)
+    (electric-cursor-set-cursor electric-cursor--original-cursor)))
 
 (provide 'electric-cursor)
-
 ;;; electric-cursor.el ends here
